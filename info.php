23de6befaafbf240c61f42ace75ba29640c4c1a6
KyleKern <kyle.z.kern@gmail.com>
2018-04-26 08:20:16 -0700

    Create info.php

---
+++ info.php
@@ -0,0 +1,56 @@
+<html>
+    <head>
+        <title>S.S.P.A.R</title>
+        <meta charset="utf-8">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="./css/styles.css" type="text/css" />
+    </head>
+    
+    <body>
+    <section class="container">
+      <div class="sscs"> 
+          <img src="./img/sscs-logo.png" alt="SSCS">
+      </div> 
+      <div class="search">
+      <form action="users.php" method="GET">
+        <input id="search" type="text" placeholder="Type here">
+        <input id="submit" type="submit" value="Search">
+      </form>
+      <h5>
+          POSsearch
+      </h5>
+      <form action="users.php" method="GET">
+        <input id="search" type="text" placeholder="Type here">
+        <input id="submit" type="submit" value="Search">
+      </form>
+     <form action="about.html">
+        <input type="submit" value="About Us">
+      </form>
+       <br/>
+     <form action="logout.php">
+     <!-- <form action="404.html">
+        <input type="submit" value="reorder soon">
+      </form>
+      -->
+      <form action="logout.php">
+        <input type="submit" value="Logout" />
+      </form>
+     </div>
+   </section>
+
+      <div class="clear"></div>
+     
+<section class="container2">
+    <center>
+   <h2 class="sub-header">YALL MIND IF I SCREAM</h2>
+   
+       
+</center>
+</section>
+    </body>
+</html>
